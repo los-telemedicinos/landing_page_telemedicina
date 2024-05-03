64f371814f04630e791c0bd5ce2396d1f9cb0583
Janny0108 <jani.1avenda@gmail.com>
2024-05-02 20:01:50 -0500

    Se agregaron los save, delete y get de los doctores

--- DocSeekerTP/src/main/java/pe/edu/upc/DocSeekerTP/Controller/DoctorController.java
+++ DocSeekerTP/src/main/java/pe/edu/upc/DocSeekerTP/Controller/DoctorController.java
@@ -3,10 +3,8 @@ package pe.edu.upc.DocSeekerTP.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResponseErrorHandler;
 import pe.edu.upc.DocSeekerTP.Services.DoctorService;
 import pe.edu.upc.DocSeekerTP.entities.Doctor;
 
@@ -17,11 +15,26 @@ import java.util.List;
 @RequestMapping("/api")
 
 public class DoctorController {
-   @Autowired
+    @Autowired
     DoctorService doctorService;
-   @GetMapping("/doctors")
-    public ResponseEntity<List<Doctor>> getDoctor(){
-       List<Doctor> doctors =doctorService.listAll();
-       return new ResponseEntity<List<Doctor>>(doctors, HttpStatus.OK);
-   }
+
+    //http://localhost:8080/api/doctors
+    @GetMapping("/doctors")
+    public ResponseEntity<List<Doctor>> getDoctor() {
+        List<Doctor> doctors = doctorService.listAll();
+        return new ResponseEntity<List<Doctor>>(doctors, HttpStatus.OK);
+    }
+
+    @PostMapping("/doctors")
+    public ResponseEntity<Doctor> createDoctor(@RequestBody Doctor doctor) {
+        Doctor newDoctor = doctorService.save(doctor);
+        return new ResponseEntity<Doctor>(newDoctor, HttpStatus.CREATED);
+    }
+
+    //Pasa el id por el body
+    @DeleteMapping("/doctors/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable("id") Long id) {
+        doctorService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
